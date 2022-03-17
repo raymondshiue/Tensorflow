0337681aef1b03bf0f1a37eb5bdf1f69e62a04d3
raymondshiue <raymondshiue@gmail.com>
2022-03-17 22:52:47 +0800

    update linear regression.py

--- linear regression.py
+++ linear regression.py
@@ -9,8 +9,14 @@ from tensorflow.keras.layers import Layer
 import matplotlib.pyplot as plt
 from datetime import datetime
 import numpy as np
+import shutil
 
-tf.compat.v1.disable_eager_execution()
+dir_path = './logs'
+
+try:
+    shutil.rmtree(dir_path)
+except OSError as e:
+    print("Error: %s : %s" % (dir_path, e.strerror))
 
 class SimpleDense(Layer):
     def __init__(self, units=32, activation=None):
@@ -19,17 +25,24 @@ class SimpleDense(Layer):
         self.activation = tf.keras.activations.get(activation)
 
     def build(self, input_shape):
-        w_init = tf.random_normal_initializer()
-        self.w = tf.Variable(name="kernel", initial_value=w_init(shape=(input_shape[-1], self.units),
-                dtype='float32'),trainable=True)
-
-        b_init = tf.zeros_initializer()
-        self.b = tf.Variable(name="bias", initial_value=b_init(shape=(self.units,), dtype='float32'), 
-        		trainable=True)
-        super().build(input_shape)
+        with tf.name_scope(self.name):
+            with tf.name_scope("weights"):
+                w_init = tf.random_normal_initializer()
+                self.w = tf.Variable(name="W", initial_value=w_init(shape=(input_shape[-1], self.units),
+                        dtype='float32'),trainable=True)
+                tf.summary.histogram(self.name +"/weights", self.w)
+            with tf.name_scope("biases"):
+                b_init = tf.zeros_initializer()
+                self.b = tf.Variable(name="b", initial_value=b_init(shape=(self.units,), dtype='float32'), 
+                		trainable=True)
+                tf.summary.histogram(self.name + "/biases", self.b)
+            super().build(input_shape)
 
     def call(self, inputs):
-        return self.activation(tf.matmul(inputs, self.w) + self.b)
+        with tf.name_scope("Wx_plus_b"):
+            outputs=tf.add(tf.matmul(inputs, self.w), self.b)
+            tf.summary.histogram(self.name + "/outputs", outputs)
+        return self.activation(outputs)
 
 class PerformancePlotCallback(tf.keras.callbacks.Callback):
     def __init__(self, x_test, lines):
@@ -37,7 +50,7 @@ class PerformancePlotCallback(tf.keras.callbacks.Callback):
         self.lines = lines
         
     def on_epoch_end(self, epoch, logs={}):
-    	if epoch%20==0:    # update plot every 20 epoch
+    	if epoch %20 ==0:    # update plot every 20 epoch
 	    	y_pred = self.model.predict(self.x_test)
 	    	try:
 	    		ax.lines.remove(self.lines[0])
@@ -53,11 +66,14 @@ ydata = np.square(xdata) -0.5 +noise
 model = tf.keras.models.Sequential([
     SimpleDense(10, activation='relu'),
     # tf.keras.layers.Dropout(0.2),
-    # tf.keras.layers.Dense(10, activation='softmax')
+    # tf.keras.layers.Dense(10, activation='softmax'),
     SimpleDense(1, activation=None)
 ])
 
-tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir = "logs/")
+for i in range(len(model.layers)):
+    model.layers[i]._name="layer%s" %(i+1)
+
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir = "logs/", histogram_freq=20)
 # tensorboard --logdir logs
 
 ax=plt.subplot(1,1,1)
@@ -66,7 +82,9 @@ lines = ax.plot(xdata,ydata)
 plt.ion()
 plt.show()
 performance_simple=PerformancePlotCallback(xdata, lines)
+
 model.compile(optimizer='sgd', loss='mean_squared_error')
-model.fit(xdata, ydata, epochs=1000,verbose=1,callbacks=[performance_simple, tensorboard_callback])
+model.fit(xdata, ydata, epochs=1000, verbose=1, 
+        callbacks=[performance_simple, tensorboard_callback])
 		#, validation_data=(xdata[:5], ydata[:5]),validation_freq=50)
 print(model.evaluate(xdata, ydata))
